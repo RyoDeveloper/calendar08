53cfb4dca6b236e2429ef90bba954cf04a529e97
RyoDeveloper <121738322+RyoDeveloper@users.noreply.github.com>
2023-02-10 23:50:03 +0900

    Initial Commit

---
+++ Noren/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  Noren
+//
+//  https://github.com/RyoDeveloper/Noren
+//  Copyright © 2023 RyoDeveloper. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Noren/NorenApp.swift
@@ -0,0 +1,18 @@
+//
+//  NorenApp.swift
+//  Noren
+//
+//  https://github.com/RyoDeveloper/Noren
+//  Copyright © 2023 RyoDeveloper. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct NorenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
