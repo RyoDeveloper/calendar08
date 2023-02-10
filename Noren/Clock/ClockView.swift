0c559f4808189675b59d0cdba7744d29237c0e2b
RyoDeveloper <121738322+RyoDeveloper@users.noreply.github.com>
2023-02-11 01:03:05 +0900

    Add: Calender,  Reminder, Clock, Settings

---
+++ Noren/Clock/ClockView.swift
@@ -0,0 +1,21 @@
+//
+//  ClockView.swift
+//  Noren
+//
+//  https://github.com/RyoDeveloper/Noren
+//  Copyright © 2023 RyoDeveloper. All rights reserved.
+//
+
+import SwiftUI
+
+struct ClockView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ClockView_Previews: PreviewProvider {
+    static var previews: some View {
+        ClockView()
+    }
+}
---
+++ Noren/Reminder/ReminderView.swift
@@ -0,0 +1,21 @@
+//
+//  ReminderView.swift
+//  Noren
+//
+//  https://github.com/RyoDeveloper/Noren
+//  Copyright © 2023 RyoDeveloper. All rights reserved.
+//
+
+import SwiftUI
+
+struct ReminderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ReminderView_Previews: PreviewProvider {
+    static var previews: some View {
+        ReminderView()
+    }
+}
