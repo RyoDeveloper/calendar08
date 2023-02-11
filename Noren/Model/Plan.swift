e1b4079c33b23dbd52cb2560f1ba2fac7ade71b1
リョウ <121738322+RyoDeveloper@users.noreply.github.com>
2023-02-11 11:13:23 +0900

    Add: Plan

---
+++ Noren/Model/Plan.swift
@@ -0,0 +1,30 @@
+//
+//  Plan.swift
+//  Noren
+//
+//  https://github.com/RyoDeveloper/Noren
+//  Copyright © 2023 RyoDeveloper. All rights reserved.
+//
+
+import EventKit
+import Foundation
+
+struct Plan {
+    var event: EKEvent?
+    var reminder: EKReminder?
+    var date: Date?
+
+    init() {}
+
+    init(_ event: EKEvent) {
+        self.event = event
+        self.reminder = nil
+        self.date = event.startDate
+    }
+
+    init(_ reminder: EKReminder) {
+        self.reminder = reminder
+        self.event = nil
+        self.date = reminder.dueDateComponents?.date
+    }
+}
